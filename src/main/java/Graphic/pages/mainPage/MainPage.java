2e2a3ce6c5e06071eaefabb200be6051ce9bee93
yasaman-kazemi <yasamankazemi79@gmail.com>
2022-04-27 15:09:20 +0430

    pages-completed

---
+++ src/main/java/Graphic/pages/mainPage/MainPage.java
@@ -0,0 +1,11 @@
+package Graphic.pages.mainPage;
+
+import Graphic.pages.MyPanel;
+
+public class MainPage extends MyPanel {
+
+
+    public MainPage(model.pages.mainPage.MainPage page) {
+        super(page);
+    }
+}
